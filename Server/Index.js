b55f1268f09bcfb38ad7d98dccf90c2ca516d0f6
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-04-30 14:42:47 +0200

    creating add todo

--- Server/Index.js
+++ Server/Index.js
@@ -6,9 +6,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 3000; // You can change the port number if needed
+const port = 3000;
+mongoose.connect("mongodb://localhost:27017/to-do-list");
 
-app.post("/add");
+app.post("/add", (req, res) => {
+  const task = req.body.task;
+  const newTask = new Task({ task });
+
+  newTask
+    .save()
+    .then(() => {
+      res.status(201).json({ message: "Task added successfully" });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to add task", details: error });
+    });
+});
 
 // Start the server
 app.listen(port, () => {
